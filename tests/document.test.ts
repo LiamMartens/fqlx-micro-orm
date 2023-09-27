2fac592ebb36ddd98550f641177d56b1dc50abdc
Liam Martens <hi@liammartens.com>
2023-09-27 16:50:07 -0400

    chore: update tests

--- tests/document.test.ts
+++ tests/document.test.ts
@@ -22,7 +22,7 @@ class PersonCollection extends Collection<
 test('Collection.byId.exists', () => {
   const collection = new PersonCollection();
   const all = collection.byId(100).exists();
-  expect(all.toFQL()).toEqual(['Person.byId("100").exists()', {}]);
+  expect(all.toFQL()).toEqual(['Person.byId(100).exists()', {}]);
 
   type CompleteSchema = z.infer<PersonCollection['completeSchema']>;
   type Test = typeof all.fqlType extends boolean ? true : false;
@@ -33,7 +33,7 @@ test('Collection.byId.delete', () => {
   const collection = new PersonCollection();
   const del = collection.byId(100).delete();
   del.operation.forced();
-  expect(del.toFQL()).toEqual(['Person.byId("100")!.delete()', {}]);
+  expect(del.toFQL()).toEqual(['Person.byId(100)!.delete()', {}]);
 
   type Test = typeof del.fqlType extends {
     ref: { coll: { name: 'Person' } };
